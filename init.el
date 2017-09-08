71820555c5583e686725e312f26197bb90ef36de
James Lee <james@substantial.com>
2017-09-08 09:06:31 -0700

    Add ediff-last-kills

--- init.el
+++ init.el
@@ -349,6 +349,18 @@ you should place your code here."
   (global-set-key (kbd "H-=") 'my-inc-frame-font-size)
   (global-set-key (kbd "H--") 'my-dec-frame-font-size)
 
+  (defun ediff-last-kills ()
+    (interactive)
+    (let ((buffer-a (get-buffer-create "*ediff-kill-a*"))
+          (buffer-b (get-buffer-create "*ediff-kill-b*")))
+      (with-current-buffer buffer-a
+        (erase-buffer)
+        (insert (nth 0 kill-ring)))
+      (with-current-buffer buffer-b
+        (erase-buffer)
+        (insert (nth 1 kill-ring)))
+      (ediff-buffers buffer-a buffer-b)))
+
   (use-package prettier-js
     :init
     (progn
